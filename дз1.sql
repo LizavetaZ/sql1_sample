55a8eef9760549cd347a37b33e16f767407f9aca
LizavetaZ <lizuska@yandex.ru>
2023-06-05 17:26:05 +0300

    add дз1

---
+++ дз1.sql
@@ -0,0 +1,4 @@
+SELECT product_name, manufacturer, price FROM myfirstdb.mobile_phones
+Where product_count > 2;
+SELECT id, product_name, manufacturer, product_count, price FROM myfirstdb.mobile_phones
+WHERE manufacturer = "Samsung";
